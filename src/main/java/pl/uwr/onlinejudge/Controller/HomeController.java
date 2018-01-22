a8c8ba981eca1c46a4cf765da646d7a1ec298179
mtszpater <mtszpater@gmail.com>
2018-01-22 21:53:40 +0100

    #1 Init app and create first rest controller

---
+++ src/main/java/pl/uwr/onlinejudge/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package pl.uwr.onlinejudge.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Hello!";
+    }
+}
